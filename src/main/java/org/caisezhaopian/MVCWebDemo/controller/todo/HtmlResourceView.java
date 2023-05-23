15b80be06e7a727a145b6de65723593afa31ae6b
caisezhaopian <72507220+caisezhaopian@users.noreply.github.com>
2023-05-23 16:48:05 +0800

    1.0版spring MVC Webapp demo 改用jdk17，spring6

--- src/main/java/org/caisezhaopian/MVCWebDemo/controller/todo/HtmlResourceView.java
+++ src/main/java/org/caisezhaopian/MVCWebDemo/controller/todo/HtmlResourceView.java
@@ -2,13 +2,15 @@ package org.caisezhaopian.MVCWebDemo.controller.todo;
 
 import java.io.File;
 import java.util.Locale;
+import java.util.Objects;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.web.servlet.view.InternalResourceView;
 
 public class HtmlResourceView extends InternalResourceView{
     @Override
-    public boolean checkResource(Locale locale) throws Exception {
-        File file = new File(this.getServletContext().getRealPath("/")+getUrl());
+    public boolean checkResource(@NotNull Locale locale) {
+        File file = new File(Objects.requireNonNull(this.getServletContext()).getRealPath("/")+getUrl());
         return file.exists();
     }
 }
